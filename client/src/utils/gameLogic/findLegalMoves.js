c8db601ebddadad5d3564c920727e3773ff8bffb
Deniorrr <kujonek95@gmail.com>
2024-05-13 23:00:12 +0200

    filtering legal moves when checked

--- client/src/utils/gameLogic/findLegalMoves.js
+++ client/src/utils/gameLogic/findLegalMoves.js
@@ -1,6 +1,10 @@
+import { generateBoardAfterMove } from "./generateBoardAfterMove";
+
+let piece;
+
 export const findLegalMoves = (i, j, board) => {
   let legalMoves = [];
-  let piece = board[i][j];
+  piece = board[i][j];
   switch (piece) {
     case "k":
       legalMoves = blackKingMoves(i, j, board);
@@ -15,8 +19,6 @@ export const findLegalMoves = (i, j, board) => {
       legalMoves = whitePawnMoves(i, j, board);
       break;
   }
-  // legalMoves.push([i - 1, j]);
-  // legalMoves.push([i - 2, j]);
   return legalMoves;
 };
 
@@ -61,9 +63,10 @@ const blackKingMoves = (i, j, board) => {
               board[move[0] + 1][move[1] + 1] !== "P" &&
               board[move[0] + 1][move[1] - 1] !== "P"
             )
-              moves.push(move);
+              addMove(board, moves, [i, j], move, false);
           } else {
-            moves.push(move);
+            addMove(board, moves, [i, j], move, false);
+            //moves.push(move);
           }
           // check if move is not on a black pawn
         }
@@ -114,9 +117,9 @@ const whiteKingMoves = (i, j, board) => {
               board[move[0] - 1][move[1] + 1] !== "p" &&
               board[move[0] - 1][move[1] - 1] !== "p"
             )
-              moves.push(move);
+              addMove(board, moves, [i, j], move, true);
           } else {
-            moves.push(move);
+            addMove(board, moves, [i, j], move, true);
           }
         }
       }
@@ -127,27 +130,39 @@ const whiteKingMoves = (i, j, board) => {
 
 const blackPawnMoves = (i, j, board) => {
   let moves = [];
-  if (board[i + 1][j] === " ") moves.push([i + 1, j]); // move one step forward
-  if (i === 1 && board[i + 2][j] === " ") moves.push([i + 2, j]); // move two steps forward
+  if (board[i + 1][j] === " ")
+    //moves.push([i + 1, j]); // move one step forward
+    addMove(board, moves, [i, j], [i + 1, j], false);
+  if (i === 1 && board[i + 2][j] === " ")
+    //moves.push([i + 2, j]); // move two steps forward
+    addMove(board, moves, [i, j], [i + 2, j], false);
   if (i + 1 < 8 && j + 1 < 8)
     if (board[i + 1][j + 1] === "P" || board[i + 1][j + 1] === "-")
-      moves.push([i + 1, j + 1]); // attack right
+      addMove(board, moves, [i, j], [i + 1, j + 1], false);
+  //moves.push([i + 1, j + 1]); // attack right
   if (i + 1 < 8 && j - 1 >= 0)
     if (board[i + 1][j - 1] === "P" || board[i + 1][j - 1] === "-")
-      moves.push([i + 1, j - 1]); // attack left
+      addMove(board, moves, [i, j], [i + 1, j - 1], false);
+  //moves.push([i + 1, j - 1]); // attack left
   return moves;
 };
 
 const whitePawnMoves = (i, j, board) => {
   let moves = [];
-  if (board[i - 1][j] === " ") moves.push([i - 1, j]); // move one step forward
-  if (i === 6 && board[i - 2][j] === " ") moves.push([i - 2, j]); // move two steps forward
+  if (board[i - 1][j] === " ")
+    //moves.push([i - 1, j]); // move one step forward
+    addMove(board, moves, [i, j], [i - 1, j], true);
+  if (i === 6 && board[i - 2][j] === " ")
+    //moves.push([i - 2, j]); // move two steps forward
+    addMove(board, moves, [i, j], [i - 2, j], true);
   if (i - 1 >= 0 && j + 1 < 8)
     if (board[i - 1][j + 1] === "p" || board[i - 1][j + 1] === "-")
-      moves.push([i - 1, j + 1]); // attack right
+      //moves.push([i - 1, j + 1]); // attack right
+      addMove(board, moves, [i, j], [i - 1, j + 1], true);
   if (i - 1 >= 0 && j - 1 >= 0)
     if (board[i - 1][j - 1] === "p" || board[i - 1][j - 1] === "-")
-      moves.push([i - 1, j - 1]); // attack left
+      //moves.push([i - 1, j - 1]); // attack left
+      addMove(board, moves, [i, j], [i - 1, j - 1], true);
   return moves;
 };
 
@@ -158,12 +173,37 @@ const isWhiteChecked = (board) => {
       if (cell === "K") whiteKingPos = [i, j];
     });
   });
-  // DODAĆ JEŚLI OUT OF BOUNDS!!!!!!
   if (board[whiteKingPos[0] - 1][whiteKingPos[1] - 1] === "p") return true;
   if (board[whiteKingPos[0] - 1][whiteKingPos[1] + 1] === "p") return true;
   return false;
 };
+const isBlackChecked = (board) => {
+  let blackKingPos = [];
+  board.forEach((row, i) => {
+    row.forEach((cell, j) => {
+      if (cell === "k") blackKingPos = [i, j];
+    });
+  });
+  if (board[blackKingPos[0] + 1][blackKingPos[1] - 1] === "P") return true;
+  if (board[blackKingPos[0] + 1][blackKingPos[1] + 1] === "P") return true;
+  return false;
+};
 
+const addMove = (board, moves, from, to, isWhite) => {
+  let _board = JSON.parse(JSON.stringify(board)); //deep copy because two dimensional array is passed by reference
+  let boardAfterMove = generateBoardAfterMove(_board, from, to);
+  if (isWhite) {
+    if (isWhiteChecked(boardAfterMove)) {
+      return;
+    }
+  }
+  if (!isWhite) {
+    if (isBlackChecked(boardAfterMove)) {
+      return;
+    }
+  }
+  moves.push(to);
+};
 //TODO
 // checked king
 // checkmate
